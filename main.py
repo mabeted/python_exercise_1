484b449d2dfc93c18f3a9af5a8a84d80fb01d791
William Oquendo <woquendo@gmail.com>
2012-02-21 17:59:04 +0100

    Adding function to check for N answer

--- main.py
+++ main.py
@@ -10,11 +10,25 @@ import sys,os
 def get_filenames(path):
     return os.listdir(path)
 
+# is the answer a N?
+def has_N(line):
+    # convert to lower and split the line by using : as separator
+    tmp_line = line.upper().split(':')
+    # check if the first field is N
+    if 'N' == tmp_line[1].strip(): return True
+    return False
+
 # main function
 def main():
     # testing
-    print get_filenames(sys.argv[1])
-
+    # print get_filenames(sys.argv[1])
+    print has_N('Sex: N')
+    print has_N('Sex: n')
+    print has_N('Sex:N')
+    print has_N('Sex:n')
+    print has_N('Sex:meme')
+    print has_N(':N')
+    print has_N('N:sex')
 
 # call main
 main()
