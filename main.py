42c91bfe4635ae26be7b1de991c8f643f8e33381
William Oquendo <woquendo@gmail.com>
2012-02-21 18:46:33 +0100

    Adding function to change the N for an M and then replace the original file

--- main.py
+++ main.py
@@ -4,31 +4,44 @@ First python exercise
 '''
 
 # import of std modules
-import sys,os
+import sys, os, shutil
 
 # function to find all filenames and returns a list
-def get_filenames(path):
+def get_filenames(path) :
     return os.listdir(path)
 
 # is the answer a N?
-def has_N(line):
+def has_N(line) :
     # convert to lower and split the line by using : as separator
-    tmp_line = line.upper().split(':')
+    tmp_line = line.replace('\n', '').replace(' ', '')
+    tmp_line = tmp_line.upper().split(':')
     # check if the first field is N
-    if 'N' == tmp_line[1].strip(): return True
+    if len(tmp_line)>1 and 'N' == tmp_line[1].strip(): return True
     return False
+
+# change N to M
+def fix_N(filename) :
+    # open tmp_file
+    tmp_filename = filename + '_tmp'
+    shutil.copyfile(filename, tmp_filename)
+    ifile = open(filename, 'r')
+    ofile = open(tmp_filename, 'w')
+    # process each line
+    for line in ifile :
+        if has_N(line):
+            line = line.replace('N', 'M')
+        ofile.write(line)
+    # move to new name
+    shutil.move(tmp_filename, filename)
+
 
 # main function
 def main():
     # testing
     # print get_filenames(sys.argv[1])
-    print has_N('Sex: N')
-    print has_N('Sex: n')
-    print has_N('Sex:N')
-    print has_N('Sex:n')
-    print has_N('Sex:meme')
-    print has_N(':N')
-    print has_N('N:sex')
+    # print has_N('Sex: N')
+    # print has_N('Sex: n')
+    fix_N(sys.argv[1])
 
 # call main
 main()
